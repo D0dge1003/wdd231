4fd8f9974b970fde73fefe5f5ec23a15c2ef2b0a
d0dge1003 <ignaciokarelearl@gmail.com>
2025-10-30 18:09:46 +0800

    Errors FIxed

---
+++ scripts/courses.js
@@ -0,0 +1,38 @@
+const courses = [
+    {
+      courseCode: "CSE 110",
+      name: "Intro to Programming",
+      credits: 2,
+      completed: true
+    },
+    {
+      courseCode: "WDD 130",
+      name: "Web Fundamentals",
+      credits: 2,
+      completed: true
+    },
+    {
+      courseCode: "CSE 111",
+      name: "Programming with Functions",
+      credits: 2,
+      completed: false
+    },
+    {
+      courseCode: "WDD 131",
+      name: "Dynamic Web Fundamentals",
+      credits: 2,
+      completed: false
+    },
+    {
+      courseCode: "CSE 210",
+      name: "Programming with Classes",
+      credits: 2,
+      completed: false
+    },
+    {
+      courseCode: "WDD 231",
+      name: "Web Frontend Development I",
+      credits: 2,
+      completed: true
+    }
+];
